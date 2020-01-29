9434ff398330d6bb75e20619ba9227a3cc24ce94
weixianfa <372301467@qq.com>
2020-01-29 19:53:22 +0800

    fxxk

--- src/lib/login/index.js
+++ src/lib/login/index.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-axios = axios.create({}) // 不影响其他 axios 的使用
+const aaa = axios.create({}) // 不影响其他 axios 的使用
 
 module.exports = class WxLogin {
   constructor(appid, secret){
@@ -7,6 +7,6 @@ module.exports = class WxLogin {
     this.secret = secret
   }
   getOpenidByCode(code){
-    return axios(`https://api.weixin.qq.com/sns/jscode2session?appid=${this.appid}&secret=${this.secret}&js_code=${code}&grant_type=authorization_code`)
+    return aaa(`https://api.weixin.qq.com/sns/jscode2session?appid=${this.appid}&secret=${this.secret}&js_code=${code}&grant_type=authorization_code`)
   }
 }
